eed68d7d77406d2576e184d545ef6b99950d94bc
Julien MARCEL <jul_marcel@hotmail.com>
2022-04-15 12:15:53 +0200

    Composant MyRouter

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './utils/store'
-import Accueil from './pages/Accueil'
-import Connexion from './pages/Connexion'
-import NavBar from './composants/NavBar'
-import Erreur404 from './composants/Erreur404'
+import MyRouter from './utils/Router'
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Accueil />} />
-        <Route path="/connexion" element={<Connexion />} />
-        <Route path="*" element={<Erreur404 />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+      <MyRouter/>
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 )
---
+++ src/utils/Router.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Accueil from '../pages/Accueil'
+import Connexion from '../pages/Connexion'
+import NavBar from '../composants/NavBar'
+import Erreur404 from '../composants/Erreur404'
+
+export default function MyRouter() {
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Accueil />} />
+                <Route path="/connexion" element={<Connexion />} />
+                <Route path="*" element={<Erreur404 />} />
+            </Routes>
+        </Router>
+    )
+}
