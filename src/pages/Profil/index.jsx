061d1ffe566976f8d23084ad740437e741617fd7
Julien MARCEL <jul_marcel@hotmail.com>
2022-04-20 23:10:50 +0200

    yarn upgrade + corrections jsx

--- src/pages/Profil/index.jsx
+++ src/pages/Profil/index.jsx
@@ -43,35 +43,35 @@ function Profil() {
                         <button className="edit-button">Edit Name</button>
                     </div>
                     {/* Placeholder data from the mockup */}
-                    <h2 class="sr-only">Accounts</h2>
-                    <section class="account">
-                        <div class="account-content-wrapper">
-                        <h3 class="account-title">Argent Bank Checking (x8349)</h3>
-                        <p class="account-amount">$2,082.79</p>
-                        <p class="account-amount-description">Available Balance</p>
+                    <h2 className="sr-only">Accounts</h2>
+                    <section className="account">
+                        <div className="account-content-wrapper">
+                        <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+                        <p className="account-amount">$2,082.79</p>
+                        <p className="account-amount-description">Available Balance</p>
                         </div>
-                        <div class="account-content-wrapper cta">
-                        <button class="transaction-button">View transactions</button>
-                        </div>
-                    </section>
-                    <section class="account">
-                        <div class="account-content-wrapper">
-                        <h3 class="account-title">Argent Bank Savings (x6712)</h3>
-                        <p class="account-amount">$10,928.42</p>
-                        <p class="account-amount-description">Available Balance</p>
-                        </div>
-                        <div class="account-content-wrapper cta">
-                        <button class="transaction-button">View transactions</button>
+                        <div className="account-content-wrapper cta">
+                        <button className="transaction-button">View transactions</button>
                         </div>
                     </section>
-                    <section class="account">
-                        <div class="account-content-wrapper">
-                        <h3 class="account-title">Argent Bank Credit Card (x8349)</h3>
-                        <p class="account-amount">$184.30</p>
-                        <p class="account-amount-description">Current Balance</p>
+                    <section className="account">
+                        <div className="account-content-wrapper">
+                        <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+                        <p className="account-amount">$10,928.42</p>
+                        <p className="account-amount-description">Available Balance</p>
                         </div>
-                        <div class="account-content-wrapper cta">
-                        <button class="transaction-button">View transactions</button>
+                        <div className="account-content-wrapper cta">
+                        <button className="transaction-button">View transactions</button>
+                        </div>
+                    </section>
+                    <section className="account">
+                        <div className="account-content-wrapper">
+                        <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+                        <p className="account-amount">$184.30</p>
+                        <p className="account-amount-description">Current Balance</p>
+                        </div>
+                        <div className="account-content-wrapper cta">
+                        <button className="transaction-button">View transactions</button>
                         </div>
                     </section>
                 </section>
